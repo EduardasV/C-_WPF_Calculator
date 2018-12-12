3dff654fe450a41c32e5f7774a9706a671dff17a
EduardasV <33314027+EduardasV@users.noreply.github.com>
2018-12-12 15:44:02 +0000

    initial commit

---
+++ Calculator_Logic/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Logic
+{
+    public class Class1
+    {
+    }
+    public class logic
+    {
+        public static void Result(string content)
+        {
+
+        }
+    }
+}
